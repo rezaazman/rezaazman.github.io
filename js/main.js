b5fa05fa98ecb3e42745854d08791b03247b23f9
Reza Azman Akbari <MacbookProRetina@users-MacBook-Pro.local>
2017-12-31 20:38:32 +0700

    Replace lagi

--- js/main.js
+++ js/main.js
@@ -1,110 +1,31 @@
-$(document).ready(function(){
-    //animasi underline navbar
-    $(".animated-underline").mouseenter(function(){
-        $(this).css("text-decoration", "underline");
-    });
-    $(".animated-underline").mouseleave(function(){
-        $(this).css("text-decoration", "none");
-    });
-    //!animasi underline navbar
+function signin(username, password){
+	if (username === "admin" && password === "admin") {
+		alert("Sign In Success. Redirect to Home.")
+		window.location = "index.html";
+	}
+	else {
+		alert("Invalid username or password.")
+	}
+}
 
-    //animasi underline navbar
-    $(".hover-red").mouseenter(function(){
-        $(this).css("color", "red");
-    });
-    $(".hover-red").mouseleave(function(){
-        $(this).css("color", "white");
-    });
-
-    $(".navbar-jumbotron").mouseenter(function(){
-        $(this).css("text-decoration", "underline");
-    });
-    $(".navbar-jumbotron").mouseleave(function(){
-        $(this).css("text-decoration", "none");
-    });
-    //!animasi underline navbar
-
-    $(window).resize(function(){
-    	var pl = 0;
-    	var pr = 0;
-    	if ($(window).width() < 600) {
-	    	pl = $(window).width() * 0;
-	    	pr = $(window).width() * 0;
-			$('.header-padding').css('padding-left', pl);
-			$('.header-padding').css('padding-right', pr);
-		}
-	    else if ($(window).width() < 750) {
-	    	pl = $(window).width() * 0.05;
-	    	pr = $(window).width() * 0.05;
-			$('.header-padding').css('padding-left', pl);
-			$('.header-padding').css('padding-right', pr);
-		}
-		else {
-			pl = $(window).width() * 0.14;
-	    	pr = $(window).width() * 0.14;
-			$('.header-padding').css('padding-left', pl);
-			$('.header-padding').css('padding-right', pr);
-		}
-
-		if ($(window).width() < 576) {
-			$('#kagebunshin').hide();
-			$('#kagebunshin-y').removeClass('mt-2').addClass('mt-0');
-		}
-		else {
-			$('#kagebunshin').show();
-			$('#kagebunshin-y').removeClass('mt-0').addClass('mt-2');
-		}
-
-		if ($(window).width() < 1200) {
-	    	pl = $(window).width() * 0.01;
-	    	pr = $(window).width() * 0.01;
-			$('.header-padding-2').css('padding-left', pl);
-			$('.header-padding-2').css('padding-right', pr);
-		}
-
-		if ($(window).width() < 992) {
-			pl = $(window).width() * 0.005;
-	    	pr = $(window).width() * 0.005;
-			$('.header-padding-2').css('padding-left', pl);
-			$('.header-padding-2').css('padding-right', pr);
-		}
-
+var map;
+function initMap() {
+	var uluru = {lat: -6.896476, lng: 107.613813};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 4,
+	  center: uluru
 	});
-
-
-$(".navbar-toggler").click(function(){
-        $('.navbar-toggler').hide();
-        $('.closebtn').css('visibility', 'visible');
-        $('#nav-item-kagebunshin').hide();
-        $('#nav-item-kagebunshin-2').hide();
-    });
-$(".closebtn").click(function(){
-        $('.navbar-toggler').show();
-        $('.closebtn').css('visibility', 'hidden');
-        $('#nav-item-kagebunshin').show(100);
-        $('#nav-item-kagebunshin-2').show(100);
-    });
-
-//tutup
-});
-
-//================================= OFF CANVAS ===============================
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+	var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});
 }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-}
 
-//===================================== Lavalamp ==============================
-$('#lampeffect').lavalamp({
-    easing: 'easeOutBack',
-    deepFocus: true
+$(document).ready(function(){
+    $('#ul-none li').hover(function() {
+        $(this).animate({ paddingLeft: '20px', color : '#111'}, 150);
+    }, function() {
+        $(this).animate({ paddingLeft: '0px', color : '#fff' }, 150);
+    });     
 });
-
-
-
